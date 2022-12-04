1f6676b6ed3e47dad1e47e3b32003e469b484e51
Irzan Luthfi <irzanluthfi@students.unnes.ac.id>
2022-12-04 22:33:14 +0700

    menambahkan komponen & edit readme

--- src/components/ProdukPage/Blazer.js
+++ src/components/ProdukPage/Blazer.js
@@ -10,95 +10,95 @@ import Blazer6 from '../../styles/images/Produk/blazer6.jpg'
 function Blazer() {
   return (
     
-    <section class="menu section-padding">
-    <div class="container">
-        <div class="row">
+    <section className="menu section-padding">
+    <div className="container">
+        <div className="row">
 
-            <div class="col-12">
-                <h2 class="mb-lg-5 mb-4">Blazer</h2>
+            <div className="col-12">
+                <h2 className="mb-lg-5 mb-4">Blazer</h2>
             </div>
 
-            <div class="col-lg-4 col-md-6 col-12">
-                <div class="menu-thumb">
-                    <img src={Blazer1} class="img-fluid menu-image" alt=""/>
+            <div className="col-lg-4 col-md-6 col-12">
+                <div className="menu-thumb">
+                    <img src={Blazer1} className="img-fluid menu-image" alt=""/>
 
-                    <div class="menu-info d-flex flex-wrap align-items-center">
-                        <h4 class="mb-0">Double Breasted Blazer</h4>
+                    <div className="menu-info d-flex flex-wrap align-items-center">
+                        <h4 className="mb-0">Double Breasted Blazer</h4>
 
-                        <span class="price-tag bg-white shadow-lg ms-4"><small>Rp</small>200.000</span>
+                        <span className="price-tag bg-white shadow-lg ms-4"><small>Rp</small>200.000</span>
 
                     </div>
                 </div>
             </div>
 
-            <div class="col-lg-4 col-md-6 col-12">
-                <div class="menu-thumb">
-                    <img src={Blazer2} class="img-fluid menu-image" alt=""/>
+            <div className="col-lg-4 col-md-6 col-12">
+                <div className="menu-thumb">
+                    <img src={Blazer2} className="img-fluid menu-image" alt=""/>
 
-                    <div class="menu-info d-flex flex-wrap align-items-center">
-                        <h4 class="mb-0">Single Breasted Blazer</h4>
+                    <div className="menu-info d-flex flex-wrap align-items-center">
+                        <h4 className="mb-0">Single Breasted Blazer</h4>
 
-                        <span class="price-tag bg-white shadow-lg ms-4"><small>Rp</small>220.000</span>
-
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 col-12">
-                <div class="menu-thumb">
-                    <img src={Blazer3} class="img-fluid menu-image" alt=""/>
-
-                    <div class="menu-info d-flex flex-wrap align-items-center">
-                        <h4 class="mb-0">Cotton Women Blazer</h4>
-
-                        <span class="price-tag bg-white shadow-lg ms-4"><small>Rp</small>250.000</span>
-
-                        <del class="ms-4"><small>Rp</small>400.000</del>
+                        <span className="price-tag bg-white shadow-lg ms-4"><small>Rp</small>220.000</span>
 
                     </div>
                 </div>
             </div>
-            
-            <div class="col-lg-4 col-md-6 col-12">
-                <div class="menu-thumb">
-                    <img src={Blazer4} class="img-fluid menu-image" alt=""/>
 
-                    <div class="menu-info d-flex flex-wrap align-items-center">
-                        <h4 class="mb-0">Slim Fit Blazer</h4>
+            <div className="col-lg-4 col-md-6 col-12">
+                <div className="menu-thumb">
+                    <img src={Blazer3} className="img-fluid menu-image" alt=""/>
 
-                        <span class="price-tag bg-white shadow-lg ms-4"><small>Rp</small>250.000</span>
+                    <div className="menu-info d-flex flex-wrap align-items-center">
+                        <h4 className="mb-0">Cotton Women Blazer</h4>
 
-                        <del class="ms-4"><small>Rp</small>400.000</del>
+                        <span className="price-tag bg-white shadow-lg ms-4"><small>Rp</small>250.000</span>
+
+                        <del className="ms-4"><small>Rp</small>400.000</del>
 
                     </div>
                 </div>
             </div>
             
-            <div class="col-lg-4 col-md-6 col-12">
-                <div class="menu-thumb">
-                    <img src={Blazer5} class="img-fluid menu-image" alt=""/>
+            <div className="col-lg-4 col-md-6 col-12">
+                <div className="menu-thumb">
+                    <img src={Blazer4} className="img-fluid menu-image" alt=""/>
 
-                    <div class="menu-info d-flex flex-wrap align-items-center">
-                        <h4 class="mb-0">Long Women Blazer</h4>
+                    <div className="menu-info d-flex flex-wrap align-items-center">
+                        <h4 className="mb-0">Slim Fit Blazer</h4>
 
-                        <span class="price-tag bg-white shadow-lg ms-4"><small>Rp</small>250.000</span>
+                        <span className="price-tag bg-white shadow-lg ms-4"><small>Rp</small>250.000</span>
 
-                        <del class="ms-4"><small>Rp</small>400.000</del>
+                        <del className="ms-4"><small>Rp</small>400.000</del>
 
                     </div>
                 </div>
             </div>
             
-            <div class="col-lg-4 col-md-6 col-12">
-                <div class="menu-thumb">
-                    <img src={Blazer6} class="img-fluid menu-image" alt=""/>
+            <div className="col-lg-4 col-md-6 col-12">
+                <div className="menu-thumb">
+                    <img src={Blazer5} className="img-fluid menu-image" alt=""/>
 
-                    <div class="menu-info d-flex flex-wrap align-items-center">
-                        <h4 class="mb-0">Formal Blazer</h4>
+                    <div className="menu-info d-flex flex-wrap align-items-center">
+                        <h4 className="mb-0">Long Women Blazer</h4>
 
-                        <span class="price-tag bg-white shadow-lg ms-4"><small>Rp</small>250.000</span>
+                        <span className="price-tag bg-white shadow-lg ms-4"><small>Rp</small>250.000</span>
 
-                        <del class="ms-4"><small>Rp</small>400.000</del>
+                        <del className="ms-4"><small>Rp</small>400.000</del>
+
+                    </div>
+                </div>
+            </div>
+            
+            <div className="col-lg-4 col-md-6 col-12">
+                <div className="menu-thumb">
+                    <img src={Blazer6} className="img-fluid menu-image" alt=""/>
+
+                    <div className="menu-info d-flex flex-wrap align-items-center">
+                        <h4 className="mb-0">Formal Blazer</h4>
+
+                        <span className="price-tag bg-white shadow-lg ms-4"><small>Rp</small>250.000</span>
+
+                        <del className="ms-4"><small>Rp</small>400.000</del>
 
                     </div>
                 </div>
--- src/components/ProdukPage/GownDress.js
+++ src/components/ProdukPage/GownDress.js
@@ -5,37 +5,37 @@ import gaun2 from '../../styles/images/Produk/gaun2.jpg'
 
 function GownDress() {
   return (
-    <section class="menu section-padding bg-white">
-    <div class="container">
-        <div class="row">
+    <section className="menu section-padding bg-white">
+    <div className="container">
+        <div className="row">
 
-            <div class="col-12">
-                <h2 class="mb-lg-5 mb-4">Gown Dress & Set Wedding</h2>
+            <div className="col-12">
+                <h2 className="mb-lg-5 mb-4">Gown Dress & Set Wedding</h2>
             </div>
 
-            <div class="col-lg-6 col-12">
-                <div class="menu-thumb">
-                    <img src={gaun1} class="img-fluid menu-image" alt=""/>
+            <div className="col-lg-6 col-12">
+                <div className="menu-thumb">
+                    <img src={gaun1} className="img-fluid menu-image" alt=""/>
 
-                    <div class="menu-info d-flex flex-wrap align-items-center">
-                        <h4 class="mb-0">Gown Set</h4>
+                    <div className="menu-info d-flex flex-wrap align-items-center">
+                        <h4 className="mb-0">Gown Set</h4>
 
-                        <span class="price-tag bg-white shadow-lg ms-4"><small>Rp</small>900.000</span>
+                        <span className="price-tag bg-white shadow-lg ms-4"><small>Rp</small>900.000</span>
 
-                        <del class="ms-4"><small>Rp</small>1.000.000</del>
+                        <del className="ms-4"><small>Rp</small>1.000.000</del>
 
                     </div>
                 </div>
             </div>
 
-            <div class="col-lg-6 col-12">
-                <div class="menu-thumb">
-                    <img src={gaun2} class="img-fluid menu-image" alt=""/>
+            <div className="col-lg-6 col-12">
+                <div className="menu-thumb">
+                    <img src={gaun2} className="img-fluid menu-image" alt=""/>
 
-                    <div class="menu-info d-flex flex-wrap align-items-center">
-                        <h4 class="mb-0">Couple Wedding Set</h4>
+                    <div className="menu-info d-flex flex-wrap align-items-center">
+                        <h4 className="mb-0">Couple Wedding Set</h4>
 
-                        <span class="price-tag bg-white shadow-lg ms-4"><small>RP</small>1.500.000</span>
+                        <span className="price-tag bg-white shadow-lg ms-4"><small>RP</small>1.500.000</span>
 
                     </div>
                 </div>
