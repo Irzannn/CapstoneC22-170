4295d8458e41086dd35dd4ba8aa566a11d7e15ff
Fernanda Naufal <20081010257@student.upnjatim.ac.id>
2022-12-07 09:22:55 +0700

    mengubah componen how to order

--- src/components/HowToOrder.js
+++ src/components/HowToOrder.js
@@ -8,18 +8,18 @@ function HowToOrder () {
 					<h1>HOW TO ORDER?</h1>
 					<div className="col-md-4 wow fadeInUp templatemo-box" data-wow-delay="0.3s">
 						<i className="fa fa-laptop"></i>
-						<h3 className="text-uppercase">1. LOGIN</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. </p>
+						<h3 className="text-uppercase">1. CHOOSE MODEL</h3>
+						<p> Pilih model baju yang diinginkan dan ukur sesuai ketentuan yang telah ditentukan</p>
 					</div>
 					<div className="col-md-4 wow fadeInUp templatemo-box" data-wow-delay="0.3s">
 						<i className="fa fa-search"></i>
 						<h3 className="text-uppercase">2. CHOOSE A TAILOR</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. </p>
+						<p>Pilih penjahit yang sesuai spesialisasi dan isi ketentuan ketentuan yang telah disepakati</p>
 					</div>
 					<div className="col-md-4 wow fadeInUp templatemo-box" data-wow-delay="0.3s">
 						<i className="fa fa-phone"></i>
 						<h3 className="text-uppercase">3. ORDER SENT</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. </p>
+						<p>Order dikirim dan tinggal menunggu pakaian tersebut datang ke rumah</p>
 					</div>
 				</div>
 			</div>
